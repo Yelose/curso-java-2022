ae5c0a40985402e527edc4b232aafae2deae0b26
González Suárez <marina.gonzalez-suarez@capgemini.com>
2022-11-25 08:00:44 +0100

    ejercicio 4 por fin

--- ej4/src/modules/MiPila.java
+++ ej4/src/modules/MiPila.java
@@ -1,14 +1,53 @@
 package modules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MiPila {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int indice = 0;
-		char[] pila = new char[6];
-		
-		
-		System.out.println(pila);
+		iniciarLista();
+		pop();
+		push("manzana");
+		peek();
+		System.out.println(list);
+		reverse();
 	}
-
+	
+	static List<String> list=new ArrayList<String>();
+	
+	static void iniciarLista() {
+		list.add("pera");
+		list.add("patata");
+		list.add("plato");
+		list.add("tenedor");
+		
+		System.out.println("lista inicial = " + list);
+	}
+	
+	static void pop() {		
+		String ultimo = list.remove(list.size() - 1);
+		System.out.println("borrado " + ultimo + ". Lista= " + list);
+	}
+	
+	
+	static void push(String e){
+		list.add(e);
+		System.out.println("push " + e + " = " + list);
+	}
+	
+	static void peek() {
+		String mostrarUltimo = list.get(list.size()-1);
+		System.out.println(mostrarUltimo);
+	}
+	
+	static void reverse() {
+		ArrayList<String> list2 = new ArrayList<String>();
+		int index = list.size();
+		for (int i = 0; i < index; i++) {
+			list2.add(list.remove(list.size() - 1));
+		}
+		list = list2;
+		System.out.println(list);
+	}
 }
